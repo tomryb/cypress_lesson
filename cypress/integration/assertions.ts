e13d5ed6a153ab87d34cc31d109f547dc4182116
tomryb <1rybak.tomasz@gmail.com>
2021-11-12 13:56:51 +0100

    add expect

--- cypress/integration/assertions.ts
+++ cypress/integration/assertions.ts
@@ -3,4 +3,9 @@ it('learning assertions', function () {
   cy.contains('get').click();
   cy.get('#query-btn').should('contain', 'Button').should('be.enabled');
   cy.get('#query-btn').invoke('attr', 'id')
+  expect(true).to.be.true
+
+  let name = 'Tom'
+
+  expect(name).to.be.equal('Tom')
 });
